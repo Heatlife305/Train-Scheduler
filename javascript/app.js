7b558b087667250f75d1cbb4fed336ac4154ec0e
Kung Fu Kenny <brianferreira74@yahoo.com>
2019-04-23 23:22:53 -0400

    initialized firebase and created button that pushes data to the firebase database

--- javascript/app.js
+++ javascript/app.js
@@ -1,20 +1,56 @@
-$(document).on("ready", function() {
 
-    var config = {
-        apiKey: "AIzaSyB7NscjJtn1YDiD10GWzKlNVvqZRs1In-A",
-        authDomain: "train-scheduler-c7973.firebaseapp.com",
-        databaseURL: "https://train-scheduler-c7973.firebaseio.com",
-        projectId: "train-scheduler-c7973",
-        storageBucket: "train-scheduler-c7973.appspot.com",
-        messagingSenderId: "256075595247"
-      };
-      firebase.initializeApp(config);
+var config = {
+    apiKey: "AIzaSyB7NscjJtn1YDiD10GWzKlNVvqZRs1In-A",
+    authDomain: "train-scheduler-c7973.firebaseapp.com",
+    databaseURL: "https://train-scheduler-c7973.firebaseio.com",
+    projectId: "train-scheduler-c7973",
+    storageBucket: "train-scheduler-c7973.appspot.com",
+    messagingSenderId: "256075595247"
+};
+firebase.initializeApp(config);
 
-    var database = firebase.database();
-    
-    database.ref().on("value", function(snapshot) {
-        console.log(snapshot.val());
+var database = firebase.database();
+
+// Button for adding train
+$("#add-train").on("click", function (event) {
+    event.preventDefault();
+
+    //Grabs user input
+    var trainName = $("#train-name").val().trim();
+    var trainDestination = $("#train-destination").val().trim();
+    var trainTime = $("#train-time").val().trim();
+    var trainFrequency = $("#train-frequency").val().trim();
+
+    // Creates local "temporary" object for holding train data
+    var newTrain = {
+        name: trainName,
+        destination: trainDestination,
+        time: trainTime,
+        frequency: trainFrequency
+    };
+
+    // Uploads employee data to the database
+    database.ref().push(newTrain);
+
+    // Logging inputs to the console
+    console.log(trainName.name);
+    console.log(trainDestination.destination);
+    console.log(trainTime.time);
+    console.log(trainFrequency.frequency);
+
+    alert("Train successfully added");
+
+        // Clears all of the text-boxes
+        $("#train-name").val("");
+        $("#train-destination").val("");
+        $("#train-time").val("");
+        $("#train-frequency").val("");
+
     });
 
-
+// 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot,val());
 });
+
+
